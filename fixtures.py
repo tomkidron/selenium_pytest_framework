e708a4b57a4988068351d4f77eba02fa9f477f78
Tom kidron <tomkidron@gmail.com>
2024-04-05 11:25:30 +0300

    changed the function into a class

--- fixtures.py
+++ fixtures.py
@@ -2,21 +2,26 @@ import pytest
 from selenium import webdriver
 
 
-@pytest.fixture(
-    scope="class", params=["chrome", "firefox"]
-)  # List of supported browsers
+class WebDriverFixture:
+    def __init__(self, browser):
+        if browser == "chrome":
+            self.driver = webdriver.Chrome()  # Chrome WebDriver
+        elif browser == "firefox":
+            self.driver = webdriver.Firefox()  # Firefox WebDriver
+        else:
+            raise ValueError(f"Unsupported browser: {browser}")
+
+        self.driver.maximize_window()
+
+    def __enter__(self):
+        return self.driver
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.driver.quit()
+
+
+@pytest.fixture(scope="class", params=["chrome", "firefox"])
 def init_webdriver_instance(request: pytest.FixtureRequest):
     browser = request.param
-
-    if browser == "chrome":
-        driver = webdriver.Chrome()  # Chrome WebDriver
-    elif browser == "firefox":
-        driver = webdriver.Firefox()  # Firefox WebDriver
-    else:
-        raise ValueError(f"Unsupported browser: {browser}")
-
-    driver.maximize_window()
-
-    yield driver
-
-    driver.quit()
+    with WebDriverFixture(browser) as driver:
+        yield driver
